a16a4ad0880d3d96828f3ab6bc6d509a6fe844d1
aYoubaitlaarbi <me@gmail.com>
2026-02-28 17:15:32 +0000

    setup web server

---
+++ services/start_web.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# this script for start a web server . i use a simple configuration , default one , i change only the ports to 8000 , 4443 on local network . 
+# services/start_web.sh
+# 
+
+
+# the name of the service
+SERVICE="apache2"
+
+# check if the service was installed
+if ! systemctl list-units --type=service | grep -q "$SERVICE"; then
+    echo "$SERVICE not installed!"
+    exit 1
+fi
+
+# start the service
+echo "Starting $SERVICE ..."
+sudo systemctl start $SERVICE
+
+
+# check if the service is active 
+STATUS=$(systemctl is-active $SERVICE)
+if [ "$STATUS" = "active" ]; then
+    echo "$SERVICE is active"
+    echo "Server ip : "
+    ip a | grep -w inet | awk '{print $2}'
+    echo "Server ports"
+    ss -tulpn | grep -E '8000|4443' | awk '{print $5}'
+else
+    echo "Failed to start $SERVICE "
+fi
